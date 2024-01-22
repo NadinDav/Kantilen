2c6f75d6103e8eb4bdc7501945ada922ed9b0769
Nadzeya <92446843+NadinDav@users.noreply.github.com>
2024-01-22 20:24:20 +0300

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
     <body>
 @include('layouts.navbar')
 @include('layouts.header')
-@include('layouts.about')
+{{--@include('layouts.about')--}}
 
 {{--        <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">--}}
 {{--            @if (Route::has('login'))--}}
